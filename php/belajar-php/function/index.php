3a744aa294aab8d5c5f147bcea858954a7b4cc41
ehanz12 <rhanss@gmail.com>
2025-01-25 15:22:58 +0700

    function

--- php/belajar-php/function/index.php
+++ php/belajar-php/function/index.php
@@ -1,34 +1,27 @@
 <?php
 
-function hitung($a, $b, $c){
-    $luas = $a * $b;
-    $keliling = $a * $b *$c;
-    return $luas . " " .$keliling;
-}
+// function hitung($a, $b, $c){
+//     $luas = $a * $b;
+//     $keliling = $a * $b *$c;
+//     return $luas . " " .$keliling;
+// }
 
-echo " dikali sama dengan </br> ". hitung(10, 5, 5);
-//pertemuan 2
-
-
-function cetaklayar($nama = "kucing"){
-    echo "hai nama saya <h1>$nama</h1>";
-}
-
-cetaklayar("farhan");
+// echo " dikali sama dengan </br> ". hitung(10, 5, 5);
+// //pertemuan 2
 
 
-function tambah ($bil1, $bil2){
-    global $hasil;
-    $bil3 =[ $bil1, $bil2, $bil1 + $bil2];
+// function cetaklayar($nama = "kucing"){
+//     echo "hai nama saya <h1>$nama</h1>";
+// }
+
+// cetaklayar("farhan");
+
+
+function tambah ($numA, $numB){
+    $bil3 = $numA . " " . $numB . " " . $numA + $numB;
     return $bil3;
-
 }
-$jawa = tambah(1,2,3);
-echo "hasilnya adalah </br>";
-echo $jawa[0];
-echo $jawa[1];
-echo $jawa[2];
 
-
-
+$jawa = tambah(1,2);
+echo "hasilnya adalah $jawa";
 ?>
