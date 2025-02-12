519eff239b953cafedb6fcfba3bb679e64a0ed31
ehanz12 <rhanss@gmail.com>
2025-02-12 09:19:30 +0700

    class

--- php/belajar-php/ClassObject/ClassMobil.php
+++ php/belajar-php/ClassObject/ClassMobil.php
@@ -1,39 +1,45 @@
 <?php
 
 class Mycar {
-    public $jenis;
+    public $mobil;
+    public $merk;
 
-    public function __construct($jenis)
-    {
-     $this->jenis = $jenis;   
+    public function __construct($mobil, $merk)
+    {   
+     $this->mobil = $mobil; 
+     $this->merk = $merk;
+     echo "Mobil jenis $mobil telah dibuat ". "dan mobil ini ". $this->merk . " <br/>";
     } 
 
-    public function Setjenis(){
-        echo $this->jenis;
+    public function info(){
+       echo "mobil : " . $this->mobil . "<br/>";
+       echo "merk : " . $this->merk . "<br/>";
     }
 }
 
 class car extends Mycar
 {
-    public $merk;
+    public $nama; 
 
-    public function __construct($merk)
-    {
-        $this->merk = $merk;
+    public function __construct($mobil, $merk, $nama){
+        parent::__construct($mobil, $merk);
+        $this->nama = $nama;
+
     }
+   public function info(){
+       echo "nama : " . $this->nama . "<br>";
+       parent::info();
 
-    public function merk(){
-        echo $this->merk;
     }
 }
 
-$jenismobil = new Mycar("Sedan");
-$jenismobil->Setjenis("ceper");
+$jenismobil = new Mycar("Sedan", "ceper");
+$merekmobil = new Mycar(" BMW", "Mercedes");
+$jenismobil->info();
+$merekmobil->info();
 
-
-
-$merekmobil = new car(" BMW");
-$merekmobil->merk("Toyota");
+$pemilik = new car("BWM", "sedan", "sabrina");
+$pemilik->info();
 
 
 ?>
